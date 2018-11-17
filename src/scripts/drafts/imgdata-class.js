3db7cc66cba7ebe5e8bc145eaa5897d26ccf3a93
Wilson <wilsonj806@gmail.com>
2018-11-17 14:26:20 -0500

    Start revising modules
    - Added imgdata-class.js
    - Added upload-class.js
    - Both files are for separating concerns even further than the current
    implementation
    - In addition both files attempt to be decoupled from other modules
      - e.g decouple 'Analyzer' from handleFiles() in Upload.js
    - Both files are drafts and are intended to be deleted after completion

---
+++ src/scripts/drafts/imgdata-class.js
@@ -0,0 +1,100 @@
+class ImageData {
+  constructor() {
+    this.pixelData = [];
+    this.rgbArr = [];
+    this.sortedRGB = [];
+    this.hslArr = [];
+    this.width = undefined;
+    this.height = undefined;
+  }
+}
+ImageData.prototype.parsePixels = () => {
+  const pixels = this.pixelData;
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    const red = pixels.data[i + 0];
+    const green = pixels.data[i + 1];
+    const blue = pixels.data[i + 2];
+    const entry = [];
+    entry.push(red);
+    entry.push(green);
+    entry.push(blue);
+    this.rgbArr.push(entry);
+  }
+  return this;
+};
+ImageData.prototype.sortRGB = () => {
+  const sort = this.rgbArr.reduce((obj, item) => {
+    /* eslint-disable no-param-reassign */
+    if (!obj[item]) {
+      obj[item] = 0;
+    }
+    obj[item] += 1;
+    return obj;
+    /* eslint-enable no-param-reassign */
+  }, {});
+  const sortArr = Object.entries(sort); // Objects not meant to hold large amounts of data
+  this.sortedRGB = sortArr;
+  return this;
+};
+ImageData.prototype.findMost = () => { // determine way to compare with the rest of the accumulator
+  const rgb = this.sortedRGB;
+  function hasLowIndex(row) {
+    return row[1] < this[1];
+  }
+  const most = rgb.reduce((acc, item, i) => {
+    if (i < 10) console.log(acc.some(hasLowIndex, item));
+    if (acc.length < 3) {
+      acc.push(item);
+    } else if (acc.some(hasLowIndex, item)) {
+      acc.pop(); // pop out last value
+      acc.push(item); // push new value
+    }
+    return acc;
+  }, []);
+  console.dir(most);
+  return this;
+};
+ImageData.prototype.rgb2Hsl = () => {
+  const pixels = this.imgData;
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    const red = pixels.data[i + 0];
+    const green = pixels.data[i + 1];
+    const blue = pixels.data[i + 2];
+    const converted = convert.rgb2hsl(red, green, blue).map((val) => {
+      const roundedVal = Math.round(val);
+      return roundedVal;
+    });
+    this.hslArr.push(converted);
+  }
+  return this;
+};
+ImageData.prototype.getLightness = () => {
+  const hsl = this.hslArr;
+  const light = hsl.reduce((obj, item) => {
+    /* eslint-disable no-param-reassign */
+    if (!obj[item[2]]) {
+      obj[item[2]] = 0;
+    }
+    obj[item[2]] += 1;
+    return obj;
+    /* eslint-enable no-param-reassign */
+  }, {});
+  const lightArr = Object.entries(light);
+  // console.dir(lightArr);
+  this.convertCSV(lightArr);
+  return this;
+};
+ImageData.convertCSV = (arr) => {
+  let csv = 'Lightness \n';
+  csv += 'light val, count \n';
+  arr.forEach((row) => {
+    csv += row.join(',');
+    csv += '\n';
+  });
+  const newEle = document.createElement('a');
+  newEle.href = `data:text/csv;charset=utf-8, ${encodeURI(csv)}`;
+  newEle.target = '_blank';
+  newEle.download = 'data.csv';
+  newEle.click();
+  return this;
+};
